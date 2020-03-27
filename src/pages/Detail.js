49afb895773a71d12857befcca57a053a21b8e64
juan <juanignaciocasais@gmail.com>
2020-03-27 17:35:24 -0300

    update https

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -18,7 +18,7 @@ export class Detail extends Component {
     state = { movie: {} }
 
     _fetchMovie ({ id }) {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
         .then(res => res.json())
         .then(movie => {
             console.log({ movie })
