2e27853f58e0a42406d62921d8ae6a2fa82d820b
juan <juanignaciocasais@gmail.com>
2020-03-27 17:13:44 -0300

    Updates

---
+++ precache-manifest.1eb7739806b4786e7a75ddde32a4165d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd8274c3f03c6a62f15642c45d253ca6",
+    "url": "/search-movies/index.html"
+  },
+  {
+    "revision": "a24fee5c5ef3424872b9",
+    "url": "/search-movies/static/css/2.b9b2dd63.chunk.css"
+  },
+  {
+    "revision": "644b9c158950c35e8c72",
+    "url": "/search-movies/static/css/main.ed65bffc.chunk.css"
+  },
+  {
+    "revision": "a24fee5c5ef3424872b9",
+    "url": "/search-movies/static/js/2.6dda3988.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/search-movies/static/js/2.6dda3988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644b9c158950c35e8c72",
+    "url": "/search-movies/static/js/main.f17d34d8.chunk.js"
+  },
+  {
+    "revision": "fa9a6f57c9b8afb48a1b",
+    "url": "/search-movies/static/js/runtime-main.5302fefa.js"
+  }
+]);
